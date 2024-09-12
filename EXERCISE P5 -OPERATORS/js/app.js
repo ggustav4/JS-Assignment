151dcf163887bb099cfb980c6776739c72d4c69c
ggustav4 <guscar395@ihmeducation.se>
2024-09-12 14:52:16 +0200

    basic P5 uppgift as damp

---
+++ EXERCISE P5 -OPERATORS/js/app.js
@@ -0,0 +1,15 @@
+let kmh = 0;
+kmh = 72
+const metersecund = 3.6;
+
+console.log(kmh /= metersecund);
+
+
+let speedinput = 72;
+let speedoutput = 0
+spedoutput = speedinput / 3.6;
+console.log(speedoutput);
+
+let minutsinput = 3;
+let secundsoutput = 0;
+console.log(secundsoutput);
