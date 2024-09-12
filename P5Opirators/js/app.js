ab5de4f7d4d99ca0b59dbb570d583ae1a1183ee2
ggustav4 <guscar395@ihmeducation.se>
2024-09-12 14:11:45 +0200

    damp P5

--- P5Opirators/js/app.js
+++ P5Opirators/js/app.js
@@ -19,3 +19,25 @@ console.log(answer);
 
 answer = firstnumber % secondnumber;
 console.log(answer);
+
+let gold = 50;
+gold + 10// sparar inte
+console.log(gold);
+
+gold = gold + 10; // sparar
+console.log(gold); // = 60
+
+gold += 10;
+console.log(gold); // 70
+
+gold *= 10;
+console.log(gold); // 700
+
+gold /= 10;
+console.log(gold); // 70
+
+gold -= 10;
+console.log(gold); // 60
+
+gold %= 10;
+console.log(gold); // 0
