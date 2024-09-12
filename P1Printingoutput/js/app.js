c5d9412465826389c2eb2fbeafedee7025ce02f6
ggustav4 <guscar395@ihmeducation.se>
2024-09-12 11:13:29 +0200

    Create P4 Variables

--- P1Printingoutput/js/app.js
+++ P1Printingoutput/js/app.js
@@ -1,2 +1,20 @@
-console.log("hello world!");
-console.log("my name is Gustav");
+let x = 6;
+let x1 = 5;
+let y = 20;
+let z = 0;
+
+console.log(x1 + x1 +x1 + x1 +x1);
+console.log("hey");
+
+while (y === 20)
+{
+  console.log("hej");
+  break;
+}
+
+if (x1 === 25)
+
+{
+  console.log("tja");
+}
+
