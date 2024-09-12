42be5cfe91ddb32e5a31f523953a8ea3abb0749d
ggustav4 <guscar395@ihmeducation.se>
2024-09-12 15:20:13 +0200

    uppdaterad verition av test uppgifter sida 1

--- EXERCISE P5 -OPERATORS/js/app.js
+++ EXERCISE P5 -OPERATORS/js/app.js
@@ -5,11 +5,23 @@ const metersecund = 3.6;
 console.log(kmh /= metersecund);
 
 
-let speedinput = 72;
-let speedoutput = 0
-spedoutput = speedinput / 3.6;
-console.log(speedoutput);
+let speedinputt = 72
+let speedoutputt = 0
+speedoutputt = speedinputt / 3.6;
+console.log(speedoutputt)
 
 let minutsinput = 3;
 let secundsoutput = 0;
+secundsoutput = minutsinput * 60;
 console.log(secundsoutput);
+
+let input = 11;
+let output = 0;
+output = input / 4
+console.log(output);
+
+let inputt = 11 ;
+let outputt = 0;
+outputt = inputt % 4;
+console.log(outputt);
+
