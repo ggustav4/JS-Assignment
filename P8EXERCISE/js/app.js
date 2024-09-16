63b52ed8424eaefc4540cdafd8830e35da356cbd
ggustav4 <guscar395@ihmeducation.se>
2024-09-16 13:49:55 +0200

    P8EXERCISE V3

--- P8EXERCISE/js/app.js
+++ P8EXERCISE/js/app.js
@@ -1,9 +1,10 @@
 
-
+const favoritnumber = 13
 const birthYear = 2010;
 const currentYear = 2024;
 let ageOfPerson = currentYear - birthYear;
 console.log(ageOfPerson);
+
 
 // Save to a let variable named isChild, when the age is from 0 till 10.
 let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
@@ -29,3 +30,22 @@ else if (isAdult){
 console.log(isChild);
 console.log(isTeenager);
 console.log(isAdult);
+console.log(favoritnumber);
+
+if (favoritnumber > ageOfPerson) {
+  console.log("favoritnumer is grater");
+}
+else if (favoritnumber === ageOfPerson) {
+  console.log("they are the same");
+}
+else {
+  console.log("age is grater");
+}
+
+if (favoritnumber % 2 === 0) {
+  console.log("even number");
+}
+
+else{
+console.log("number is odd");
+}
