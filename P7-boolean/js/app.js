fdc3b92136797c5ff0f418e104fcddf00c449750
ggustav4 <guscar395@ihmeducation.se>
2024-09-16 11:43:35 +0200

    uppdaterad P7 v2

--- P7-boolean/js/app.js
+++ P7-boolean/js/app.js
@@ -1,13 +1,29 @@
-const birthYear = 2000;
-const currentyers = 2024
-let ageofperson = currentyers - birthYear;
+//const birthYear = 2000;
+//const currentyers = 2024
+//let ageofperson = currentyers - birthYear;
 
-let isadult = ageofperson >= 18;
-let isachild = ageofperson <= 10 || ageofperson >= 0;
-let isteenager = ageofperson >= 11 ||ageofperson <=17;
+//let isadult = ageofperson >= 18;
+//let isachild = ageofperson <= 10 || ageofperson >= 0;
+//let isteenager = ageofperson >= 11 ||ageofperson <=17;
 
-console.log(isadult);
-console.log(isachild);
-console.log(isteenager);
+//console.log(isadult);
+//console.log(isachild);
+//console.log(isteenager);
 
+let playertouchground = true;
 
+let playerhavejetpck = true;
+
+if (playertouchground) {
+  //när de är sant
+  console.log("player is on the ground");
+  console.log("player can jump");
+}
+else if (playerhavejetpck) { // körs när jetpack är = true
+  console.log("player can jet away");
+}
+else {
+  // när de är falskt && och har jetpack = false
+  console.log("player is not on the ground");
+  console.log("player cannot jump");
+}
