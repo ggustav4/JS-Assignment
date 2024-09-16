873750695a44fe734a35536d6400fd014f6b9821
ggustav4 <guscar395@ihmeducation.se>
2024-09-16 13:23:19 +0200

    P8EXERCISE

---
+++ P8EXERCISE/js/app.js
@@ -0,0 +1,31 @@
+
+
+const birthYear = 2010;
+const currentYear = 2024;
+let ageOfPerson = currentYear - birthYear;
+console.log(ageOfPerson);
+
+// Save to a let variable named isChild, when the age is from 0 till 10.
+let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
+
+// Save to a let variable named isTeenager, when the age is from 11 to 17
+let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
+
+// Save to a let variable named isAdult, when the age is 18 or more.
+let isAdult =  ageOfPerson >= 18;
+
+if  (isChild) {
+  console.log( isChild =  "you are a child");
+}
+
+else if (isTeenager) {
+  console.log( isTeenager =  "you are a teenager");
+}
+
+else if (isAdult){
+  console.log("you are a adult ");
+}
+
+console.log(isChild);
+console.log(isTeenager);
+console.log(isAdult);
