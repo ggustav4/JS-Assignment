d465922efb97dbcf3a4e53da50408d4d68da4045
ggustav4 <guscar395@ihmeducation.se>
2024-09-16 11:05:17 +0200

    uppgift 1 i P7

---
+++ P7-boolean/js/app.js
@@ -0,0 +1,13 @@
+const birthYear = 2000;
+const currentyers = 2024
+let ageofperson = currentyers - birthYear;
+
+let isadult = ageofperson >= 18;
+let isachild = ageofperson <= 10 || ageofperson >= 0;
+let isteenager = ageofperson >= 11 ||ageofperson <=17;
+
+console.log(isadult);
+console.log(isachild);
+console.log(isteenager);
+
+
