c46fb0ddac54df0d0c1088310972de5f2be94b26
ggustav4 <guscar395@ihmeducation.se>
2024-09-17 11:33:29 +0200

    P-10 nya uppdatering

--- P10inputandoutput/js/app.js
+++ P10inputandoutput/js/app.js
@@ -16,7 +16,25 @@
 //}
 //console.log(namn('Ben', ' Ting'))
 
-function getRandomNumber(min = 0, max = 2) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+//function getRandomNumber(min = 0, max = 2) {
+ // return Math.floor(Math.random() * (max - min + 1)) + min;
+//}
+//console.log(getRandomNumber(0, 2));  // Output will be either 0, 1, or 2() {
+
+//function getrandomint() {
+ // return Math.floor(Math.random()* 6);
+//}
+//console.log(getrandomint()); // ANNAT SÄTT ATT GÖRA RANDOM GENERATOR PÅ
+
+function convertnumbertoletter(number) {
+  if (number === 0) {
+    return "A"
 }
-console.log(getRandomNumber(0, 2));  // Output will be either 0, 1, or 2
+if (number === 1) {
+  return "B";
+}
+if (number === 2) {
+return "C"}
+}
+
+console.log(convertnumbertoletter(2));
